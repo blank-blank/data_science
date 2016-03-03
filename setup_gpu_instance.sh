753689df8d9b57da90f05fb69b8982d38176c71d
tim <tim@Lindys-MacBook-Pro.local>
2016-03-02 23:59:58 -0700

    ansible and git installed on gpu machine

---
+++ setup_gpu_instance.sh
@@ -0,0 +1,25 @@
+#=======================================================================
+# need to get git, pip, ansible installed
+#=======================================================================
+
+apt-get update
+apt-get install python-setuptools
+apt-get install git
+
+easy_install pip
+
+git clone git://github.com/ansible/ansible.git --recursive
+
+cd ./ansible
+
+source ./hacking/env-setup
+
+pip install paramiko PyYAML Jinja2 httplib2 six
+
+pip install ansible
+
+
+
+
+
+
