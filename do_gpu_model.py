318a696e03beeaf426968873a7b444e7b72a7c7d
tim <tim@Lindys-MacBook-Pro.local>
2016-03-02 23:43:45 -0700

    working script to launch a gpu based aws instance

--- do_gpu_model.py
+++ do_gpu_model.py
@@ -1,14 +1,24 @@
+#=========================================================================
+# Purpose: Boulder Data Sceince -> Execute kaggle data science bowl model 
+#           on gpu instance  -> AWS
+# Date: 20160302
+# Author: Tim Lewis
+#=========================================================================
+
+
+import argparse
+
 import boto3
-ec2 = boto3.resource('ec2')
-
-print ec2
-
-print dir(ec2)
-
 
 
 #launch the instance
-instance_type = 'g2.2xlarge'
+
+
+#boto3 resource
+
+#create images with params
+
+
 
 
 #do the things
@@ -17,7 +27,7 @@ instance_type = 'g2.2xlarge'
 
 #run the model
 
-
+#============================================================
 
 def test():
 
@@ -26,3 +36,53 @@ def test():
     #This function will not call anything that writes to anything else.  Output only
     pass
 
+#=========================================================================
+def main():
+
+    #============================================================
+
+
+    def start_gpu_machine(ami_id, instance_type, kp_name): 
+        '''
+        create a gpu aws machine running ubuntu
+        '''
+        ec2 = boto3.resource('ec2', 'us-west-1')
+
+
+  
+
+        #start gpu instance        
+        instances = ec2.create_instances(ImageId=ami_id, MinCount=1, MaxCount=1, KeyName=kp_name, InstanceType=instance_type)
+
+        return instances
+
+    #============================================================
+
+    instance_type = 'g2.2xlarge'
+    ami_id = 'ami-1d689c59' #ubuntu 14.04 lts
+    kp_name = 'bds_kaggle_cal'
+ 
+
+
+
+
+    #spin up instance and print it
+    instances = start_gpu_machine(ami_id, instance_type, kp_name) 
+    print 'creating instance in us-west-1 with ami: %s; instance_type: %s; kp_name: %s' %(ami_id, instance_type, kp_name)
+
+    print instances[0].instance_id
+
+    
+    
+
+
+
+    
+
+
+   
+
+#=========================================================================
+
+if __name__ == '__main__':
+    main()
