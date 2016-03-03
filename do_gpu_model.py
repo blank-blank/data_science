acc601162eebbdcd388b93cc318b162e07bd9039
tim <tim@Lindys-MacBook-Pro.local>
2016-03-02 21:43:15 -0700

    basic skeleton of aws gpu repo

---
+++ do_gpu_model.py
@@ -0,0 +1,23 @@
+import boto3
+ec2 = boto3.resource('ec2')
+
+print ec2
+
+print dir(ec2)
+
+
+
+#launch the instance
+instance_type = 'g2.2xlarge'
+
+
+#do the things
+
+
+
+#run the model
+
+
+
+
+
---
+++ setup.py
@@ -0,0 +1,32 @@
+#=======================================================================
+# set up the machine to run kaggle comp model on gpu instance in AWS ec2
+# goal: start an EC2 instance from AWS, load a model on the instance 
+# kaggle competition for BDS
+# Author: Tim Leiws
+#=======================================================================
+
+
+def test_dependencies_installed():
+
+    import boto3
+
+    #todo: need boto3 at least, maybe ansible, maybe paramiko?
+
+    
+
+
+
+
+def main():
+
+        
+    '''
+    desc:
+        run an instance on aws with boto3
+    req: 
+        keras -> theano -> gpu -> aws -> boto3 -> pip
+    '''
+
+    
+if __name__ == '__main__':
+    main()
